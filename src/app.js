709cb5f27e18d4536066e1f647321845f808e207
Jared Smith <j.danielsmith451@gmail.com>
2019-05-22 15:16:41 -0700

    Finished scaffolding and ready for deployment

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
+
+routeConfig.init(app);
+appConfig.init(app, express);
+
+module.exports = app;
---
+++ src/config/main-config.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const expressValidator = require("express-validator");
+const flash = require("express-flash");
+//const passportConfig = require("./passport-config");
+const logger = require("morgan");
+
+
+module.exports = {
+  init(app, express){
+    app.use(bodyParser.urlencoded({ extend: true}));
+    app.use(expressValidator());
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(session( {
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1.21e+9 }
+    }));
+    app.use(flash());
+  //  passport.config.init(app);
+    app.use((req, res, next) => {
+      res.locals.notice = req.flash("notice");
+      next();
+    });
+    app.use(logger("dev"));
+  }
+};
